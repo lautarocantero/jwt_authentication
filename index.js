5242b2841c3c23b77dbac0d7802ff82083d37aaa
lautaro <lautaroncantero@gmail.com>
2025-10-28 18:45:02 -0300

    added logic

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.json()) 
+
 app.get('/', (request, response) => {
-  response.send('Hello World!')
+  response.render('protected', { username: 'lautaro' })
 })
+
+app.post('/login', async (req,res)=>  {
+  const { username, password } = req.body;
+
+  try{
+    const user = await UserRepository.login({username, password})
+    res.send({ user }) 
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+
+})
+
+app.post('/register', async (req,res)=>  {
+  const { username, password } = req.body
+ 
+  try{
+    const id = await UserRepository.create({username, password})
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+
+})
+
+app.post('/logout', (req,res)=> {})
+
+app.get('/protected', (req,res) => {})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
