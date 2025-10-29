ec42a623ac9540189d83a91df409ccad3d84f84e
lautaro <lautaroncantero@gmail.com>
2025-10-28 21:00:40 -0300

    added jwt

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 import express from 'express'
-import { PORT } from './config.js'
+import { PORT, SECRET_JWT_KEY } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(express.json()) 
 
-app.get('/', (request, response) => {
-  response.render('protected', { username: 'lautaro' })
+app.use(express.json()) 
+app.use(cookieParser())
+
+app.use((req,res,next) => {
+  const token = req.cookies.access_token;
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY);
+    req.session.user = data;
+  } catch {}
+
+  next()
 })
 
+app.get('/', (request, response) => {
+  const { user } = request.session
+  response.render('index', user)
+})
+  
 app.post('/login', async (req,res)=>  {
   const { username, password } = req.body;
 
   try{
     const user = await UserRepository.login({username, password})
-    res.send({ user }) 
+    const token = jwt.sign({ id: user._id, username: user.username}, SECRET_JWT_KEY, {
+      expiresIn: '1h'
+    })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60
+      })
+      .send({ user, token }) 
   } catch (error) {
     res.status(401).send(error.message)
   }
@@ -35,9 +62,19 @@ app.post('/register', async (req,res)=>  {
 
 })
 
-app.post('/logout', (req,res)=> {})
+app.post('/logout', (req,res)=> {
+  res
+    .clearCookie('access_token')
+    .json({ message: 'Logout successful'})
+})
 
-app.get('/protected', (req,res) => {})
+app.get('/protected', (req,res) => {
+
+  const { user } = req.session;
+  if (!user) return res.status(403).send('Acces not authorized');
+
+  res.render('protected', user )
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
